99a992baf3d21650bfc3c857f44b6a67eef22978
liffert <digitizenetwork@gmail.com>
2020-06-29 12:37:57 +0300

    move when start check to controller and delete it from main

--- main.cpp
+++ main.cpp
@@ -4,12 +4,7 @@
 int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
     Controller controller("telegramkey.txt", "TranslatorApiSettings.csv");
-    const QTime whenStart(10, 0, 0, 0);
-    while(1){
-        if(QTime::currentTime() >= whenStart){
-            controller.startWorkingDay();
-        }
-    }
+    controller.startWorkingDay();
     
     return a.exec();
 }
